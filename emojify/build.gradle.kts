c4edfa04f377fa2dea627a2b0f48077ddacf615e
Maxwell <mxt.developer@gmail.com>
2020-06-18 20:21:50 +0200

    Add kotlinx-serialization runtime dependency

--- emojify/build.gradle.kts
+++ emojify/build.gradle.kts
@@ -2,8 +2,10 @@ import io.wax911.emoji.buildSrc.Libraries
 
 plugins {
     id("io.wax911.emojify")
+    id("kotlinx-serialization")
 }
 
 dependencies {
     implementation(Libraries.Google.Gson.gson)
+    implementation(Libraries.JetBrains.KotlinX.Serialization.runtime)
 }
