860f9262d63821ef681f84c21a8238f2f5069d86
Maxwell <mxt.developer@gmail.com>
2021-03-14 15:46:51 +0200

    Bump version from 1.6.0-alpha01 to 1.6.0-beta01

--- buildSrc/src/main/java/io/wax911/emoji/buildSrc/common/Versions.kt
+++ buildSrc/src/main/java/io/wax911/emoji/buildSrc/common/Versions.kt
@@ -6,8 +6,8 @@ object Versions {
     const val targetSdk = 29
     const val minSdk = 21
 
-    const val versionCode = 16001
-    const val versionName = "1.6.0-alpha01"
+    const val versionCode = 160010
+    const val versionName = "1.6.0-beta01"
 
     const val mockk = "1.10.0"
     const val junit = "4.13"
