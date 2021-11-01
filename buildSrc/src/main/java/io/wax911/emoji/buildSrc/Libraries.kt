5868517a78c6829e54bc005d6c495f020f7a217a
Maxwell <mxt.developer@gmail.com>
2021-11-01 13:38:13 +0200

    Bump androidx.recyclerview:recyclerview [1.2.0-rc01 -> 1.2.0]

--- buildSrc/src/main/java/io/wax911/emoji/buildSrc/Libraries.kt
+++ buildSrc/src/main/java/io/wax911/emoji/buildSrc/Libraries.kt
@@ -53,7 +53,7 @@ object Libraries {
         }
 
         object Recycler {
-            private const val version = "1.2.0-rc01"
+            private const val version = "1.2.0"
             const val recyclerView = "androidx.recyclerview:recyclerview:$version"
             const val recyclerViewSelection = "androidx.recyclerview:recyclerview-selection:$version"
         }
