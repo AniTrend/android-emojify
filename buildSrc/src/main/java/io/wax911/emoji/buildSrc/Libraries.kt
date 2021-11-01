e3ba4f7a4cfe2fcb58a1ccd0660670c2224759a7
Maxwell <mxt.developer@gmail.com>
2021-11-01 13:39:37 +0200

    Bump org.jetbrains.kotlinx:kotlinx-coroutines-* [1.4.3 to 1.5.2]

--- buildSrc/src/main/java/io/wax911/emoji/buildSrc/Libraries.kt
+++ buildSrc/src/main/java/io/wax911/emoji/buildSrc/Libraries.kt
@@ -126,7 +126,7 @@ object Libraries {
 
         object KotlinX {
             object Coroutines {
-                private const val version = "1.4.3"
+                private const val version = "1.5.2"
                 const val core = "org.jetbrains.kotlinx:kotlinx-coroutines-core:$version"
                 const val android = "org.jetbrains.kotlinx:kotlinx-coroutines-android:$version"
                 const val test = "org.jetbrains.kotlinx:kotlinx-coroutines-test:$version"
