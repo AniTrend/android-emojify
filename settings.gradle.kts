fb7f1b80c874a2a60c7a64fd0288c1c2a6ac7590
Yoobi <>
2024-03-04 08:30:17 +0100

    feat: update readme + settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,6 +2,8 @@ rootProject.name = "android-emojify"
 include(":emojify")
 include(":contract")
 include(":serializer:kotlinx")
+include(":serializer:gson")
+include(":serializer:moshi")
 
 if (!System.getenv().containsKey("CI"))
     include(":app")
