bdd197e5b0321cfec32c7fd1f8cf53d60b396de8
Maxwell Mapako <wax911@users.noreply.github.com>
2025-11-27 16:18:42 +0200

    build: update compileSdkVersion and targetSdk to 36

--- buildSrc/src/main/java/io/wax911/emoji/buildSrc/plugin/components/AndroidConfiguration.kt
+++ buildSrc/src/main/java/io/wax911/emoji/buildSrc/plugin/components/AndroidConfiguration.kt
@@ -48,10 +48,10 @@ private fun DefaultConfig.applyAdditionalConfiguration(project: Project) {
 }
 
 internal fun Project.configureAndroid(): Unit = baseExtension().run {
-    compileSdkVersion(34)
+    compileSdkVersion(36)
     defaultConfig {
         minSdk = 21
-        targetSdk = 34
+        targetSdk = 36
         versionCode = props[PropertyTypes.CODE].toInt()
         versionName = props[PropertyTypes.VERSION]
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
