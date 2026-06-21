d05f27c139474ff2d7fa2fffa9fea0f4c10af08a
Maxwell Mapako <wax911@users.noreply.github.com>
2026-06-21 10:24:13 +0200

    fix(dokka): migrate to Dokka V2 API to resolve CI failure (#518)
    * fix(dokka): migrate to Dokka V2 API to resolve CI failure
    
    Replace V1 tasks (DokkaMultiModuleTask, DokkaTask, dokkaHtmlMultiModule)
    with V2 API (DokkaExtension, dokkaGenerate, dokka() dependency config).
    
    - Root build.gradle.kts: use dokka { dokkaPublications.html {} } + dependencies { dokka(project()) }
    - AndroidOptions.kt: use extensions.configure<DokkaExtension> with V2 DSL
    - Suppress test/androidTest source sets to avoid Kotlin platform cast error
    - Android-ci.yml: replace dokkaHtmlMultiModule with :dokkaGenerate
    - ProjectExtensions.kt: remove 8 unused extension functions (dead code)
    
    * fix(dokka): suppress variant source sets, document only main
    
    Change source set filter from suppressing test/androidTest to
    documenting only "main" and suppressing all variant source sets
    (debug, release, test*, androidTest*). This fixes:
    
    1. "More than one source set with common roots" error from Dokka V2
       when debug and release variants share main source roots
    2. Kotlin/AGP 9 metadata transformation cast error for test variants
    
    ---------
    
    Co-authored-by: home-lab <home-lab@homelab>

--- buildSrc/src/main/java/io/wax911/emoji/buildSrc/plugin/components/AndroidOptions.kt
+++ buildSrc/src/main/java/io/wax911/emoji/buildSrc/plugin/components/AndroidOptions.kt
@@ -11,10 +11,10 @@ import org.gradle.api.tasks.bundling.Jar
 import org.gradle.kotlin.dsl.get
 import org.gradle.kotlin.dsl.getValue
 import org.gradle.kotlin.dsl.invoke
+import org.gradle.kotlin.dsl.configure
 import org.gradle.kotlin.dsl.named
-import org.jetbrains.dokka.Platform
-import org.jetbrains.dokka.gradle.DokkaTask
-import java.net.URL
+import org.jetbrains.dokka.gradle.DokkaExtension
+import org.jetbrains.dokka.gradle.engine.parameters.VisibilityModifier
 
 private fun Project.dependenciesOfProject(): List<Modules.Module> {
     return when (project.name) {
@@ -67,114 +67,37 @@ private fun Project.createMavenPublicationUsing(sourcesJar: Jar) {
     }
 }
 
-private fun Project.createDokkaTaskProvider() = tasks.named<DokkaTask>("dokkaHtml") {
-    outputDirectory.set(project.layout.buildDirectory.dir("docs/dokka").get())
-
-    // Set module name displayed in the final output
-    moduleName.set(this@createDokkaTaskProvider.name)
-
-    // Use default or set to custom path to cache directory
-    // to enable package-list caching
-    // When this is set to default, caches are stored in $USER_HOME/.cache/dokka
-    //cacheRoot.set(file("default"))
-
-    dokkaSourceSets {
-        configureEach { // Or source set name, for single-platform the default source sets are `main` and `test`
-            // Used when configuring source sets manually for declaring which source sets this one depends on
-            //dependsOn(dependenciesOfProject().map(Modules.Module::path))
-
-            // Used to remove a source set from documentation, test source sets are suppressed by default
-            suppress.set(false)
-
-            // Used to prevent resolving package-lists online. When this option is set to true, only local files are resolved
-            offlineMode.set(false) // this is failing in the ci env
-
-            // Use to include or exclude non public members
-            includeNonPublic.set(false)
-
-            // Do not output deprecated members. Applies globally, can be overridden by packageOptions
+private fun Project.createDokkaConfiguration() {
+    extensions.configure(DokkaExtension::class.java) {
+        moduleName.set(project.name)
+        dokkaSourceSets.configureEach {
+            suppress.set(name != "main")
             skipDeprecated.set(false)
-
-            // Emit warnings about not documented members. Applies globally, also can be overridden by packageOptions
             reportUndocumented.set(true)
-
-            // Do not create index pages for empty packages
             skipEmptyPackages.set(true)
-
-            // This name will be shown in the final output
-            //displayName.set("JVM")
-
-            // Platform used for code analysis. See the "Platforms" section of this readme
-            platform.set(org.jetbrains.dokka.Platform.jvm)
-
-            // Property used for manual addition of files to the classpath
-            // This property does not override the classpath collected automatically but appends to it
-            // classpath.from(file("libs/dependency.jar"))
-
-            // List of files with module and package documentation
-            // https://kotlinlang.org/docs/reference/kotlin-doc.html#module-and-package-documentation
-            //includes.from("packages.md", "extra.md")
-
-            // List of files or directories containing sample code (referenced with @sample tags)
-            //samples.from("samples/basic.kt", "samples/advanced.kt")
-
-            // By default, sourceRoots are taken from Kotlin Plugin and kotlinTasks, following roots will be appended to them
-            // Repeat for multiple sourceRoots
-            sourceRoot(file("src"))
+            documentedVisibilities.set(setOf(VisibilityModifier.Public))
+            sourceRoots.from(file("src"))
 
             dependenciesOfProject().forEach { module ->
-                // Specifies the location of the project source code on the Web.
-                // If provided, Dokka generates "source" links for each declaration.
-                // Repeat for multiple mappings
                 sourceLink {
-                    // Unix based directory relative path to the root of the project (where you execute gradle respectively).
                     localDirectory.set(file("src/main/kotlin"))
-
-                    val repository = "https://github.com/anitrend/android-emojify/tree/develop"
-                    // URL showing where the source code can be accessed through the web browser
-                    remoteUrl.set(URL("$repository/${module.id.replace(":", "/")}/src/main/kotlin"))
-                    // Suffix which is used to append the line number to the URL. Use #L for GitHub
+                    remoteUrl("https://github.com/anitrend/android-emojify/tree/develop/${module.id.replace(":", "/")}/src/main/kotlin")
                     remoteLineSuffix.set("#L")
                 }
             }
 
-            // Used for linking to JDK documentation
-            jdkVersion.set(21)
-
-            platform.set(Platform.jvm)
-
-            // Disable linking to online kotlin-stdlib documentation
-            noStdlibLink.set(false)
-
-            // Disable linking to online JDK documentation
-            noJdkLink.set(false)
-
-            // Disable linking to online Android documentation (only applicable for Android projects)
-            noAndroidSdkLink.set(false)
-
-            // Allows linking to documentation of the project"s dependencies (generated with Javadoc or Dokka)
-            // Repeat for multiple links
-            externalDocumentationLink {
-                // Root URL of the generated documentation to link with. The trailing slash is required!
-                url.set(URL("https://developer.android.com/reference/kotlin/"))
-
-                // If package-list file is located in non-standard location
-                packageListUrl.set(URL("https://developer.android.com/reference/androidx/package-list"))
+            externalDocumentationLinks.register("android-ref") {
+                url("https://developer.android.com/reference/kotlin/")
+                packageListUrl("https://developer.android.com/reference/androidx/package-list")
             }
 
-            // Allows to customize documentation generation options on a per-package basis
-            // Repeat for multiple packageOptions
-            // If multiple packages match the same matchingRegex, the longuest matchingRegex will be used
             perPackageOption {
-                matchingRegex.set("kotlin($|\\.).*") // will match kotlin and all sub-packages of it
-                // All options are optional, default values are below:
+                matchingRegex.set("kotlin($|\\.).*")
                 skipDeprecated.set(false)
-                reportUndocumented.set(true) // Emit warnings about not documented members
-                includeNonPublic.set(false)
+                reportUndocumented.set(true)
             }
-            // Suppress a package
             perPackageOption {
-                matchingRegex.set(".*\\.internal.*") // will match all .internal packages and sub-packages
+                matchingRegex.set(".*\\.internal.*")
                 suppress.set(true)
             }
         }
@@ -185,7 +108,7 @@ internal fun Project.configureOptions() {
     if (isLibraryModule()) {
         println("Applying additional tasks options for dokka and javadoc on ${project.path}")
 
-        createDokkaTaskProvider()
+        createDokkaConfiguration()
 
         val sourcesJar by tasks.register("sourcesJar", Jar::class.java) {
             archiveClassifier.set("sources")
